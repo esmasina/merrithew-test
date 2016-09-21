4da20baeeda7b8f02c1ef58de6e5719a4d494c32
esmasina <esma.sina@gmail.com>
2016-09-20 22:09:02 -0400

    clean up location model

--- merrithew-test/Models/Location.cs
+++ merrithew-test/Models/Location.cs
@@ -19,21 +19,5 @@ namespace merrithew_test.Models
         public decimal Lat { get; set; }
         public decimal Long { get; set; }
 
-        public static Location FromCsv(string csvLine)
-        {
-            string[] values = csvLine.Replace("\"", "").Split(',');
-            Location loc = new Location();
-            loc.LocationTypeCode = values[0];
-            loc.LocationTypeDesc = values[1];
-            loc.LocationCode = values[2];
-            loc.LocationDesc = values[3];
-            loc.LocationName = values[4];
-            loc.Address = values[5];
-            loc.Phone = values[6];
-            loc.Notes = values[7];
-            loc.Lat = Convert.ToDecimal(values[8]);
-            loc.Long = Convert.ToDecimal(values[9]);
-            return loc;
-        }
     }
 }
